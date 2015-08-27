eacc58d956092ab9a3ed3f5d071106528867147a
Elahe Jalalpour <el.jalalpour@gmail.com>
2015-08-27 19:28:57 +0430

    Resources were restructured.

--- RiverRider/src/main/java/me/elahe/riverrider/Drawing.java
+++ RiverRider/src/main/java/me/elahe/riverrider/Drawing.java
@@ -19,7 +19,6 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -28,74 +27,83 @@ public class Drawing extends JPanel {
 	int[][] map;
 	int[][] p;
 	public static int[][] m = new int[7][10];
-	BufferedImage grass;
-	BufferedImage myplane;
-	BufferedImage enemy;
-	BufferedImage zamin;
-	BufferedImage fuel;
-	BufferedImage missile;
-	BufferedImage lost;
-	BufferedImage stop;
-	BufferedImage win;
+	private BufferedImage grass;
+	private BufferedImage myPlane;
+	private BufferedImage enemy;
+	private BufferedImage ground;
+	private BufferedImage fuel;
+	private BufferedImage missile;
+	private BufferedImage lost;
+	private BufferedImage stop;
+	private BufferedImage win;
 	public static int t;
 	public static int k = 1000;
 	public static int he = 3;
-	public static int enemyl;
+	public static int enemy_l;
 
 	public static ArrayList<missiles> mis;
-	private Timer timer = new Timer(800, new ActionListener() {
-
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			missiles ma;
-			ma = new missiles(Drawing.he, 9);
-			new Thread(ma).start();
-			mis.add(ma);
-
-		}
-	});
 
 	public Drawing(int[][] map, int[][] p) {
+		Timer timer = new Timer(800, new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				missiles ma;
+				ma = new missiles(Drawing.he, 9);
+				new Thread(ma).start();
+				mis.add(ma);
+
+			}
+		});
 		timer.start();
-		mis = new ArrayList<missiles>();
+		mis = new ArrayList<>();
 
 		this.map = map;
 		this.p = p;
 		try {
-			win = ImageIO.read(new File("win.jpg"));
+			win = ImageIO.read(ClassLoader.getSystemResourceAsStream("win.jpg"));
 		} catch (IOException e) {
+			e.printStackTrace();
 		}
 		try {
-			stop = ImageIO.read(new File("stop.jpg"));
+			stop = ImageIO.read(ClassLoader.getSystemResourceAsStream("stop.jpg"));
 		} catch (IOException e) {
+			e.printStackTrace();
 		}
 		try {
-			lost = ImageIO.read(new File("lost.jpg"));
+			lost = ImageIO.read(ClassLoader.getSystemResourceAsStream("lost.jpg"));
 		} catch (IOException e) {
+			e.printStackTrace();
 		}
 		try {
-			grass = ImageIO.read(new File("grass.jpg"));
+			grass = ImageIO.read(ClassLoader.getSystemResourceAsStream("grass.jpg"));
 		} catch (IOException e) {
+			e.printStackTrace();
 		}
 		try {
-			myplane = ImageIO.read(new File("mplane.png"));
+			myPlane = ImageIO.read(ClassLoader.getSystemResourceAsStream("myPlane.png"));
 		} catch (IOException e) {
+			e.printStackTrace();
 		}
 		try {
-			enemy = ImageIO.read(new File("enemy1.png"));
+			enemy = ImageIO.read(ClassLoader.getSystemResourceAsStream("enemy_1.png"));
 		} catch (IOException e) {
+			e.printStackTrace();
 		}
 		try {
-			zamin = ImageIO.read(new File("2.png"));
+			ground = ImageIO.read(ClassLoader.getSystemResourceAsStream("ground.png"));
 		} catch (IOException e) {
+			e.printStackTrace();
 		}
 		try {
-			fuel = ImageIO.read(new File("4.png"));
+			fuel = ImageIO.read(ClassLoader.getSystemResourceAsStream("fuel.png"));
 		} catch (IOException e) {
+			e.printStackTrace();
 		}
 		try {
-			missile = ImageIO.read(new File("Rocket.png"));
+			missile = ImageIO.read(ClassLoader.getSystemResourceAsStream("Rocket.png"));
 		} catch (IOException e) {
+			e.printStackTrace();
 		}
 		setVisible(true);
 	}
@@ -106,14 +114,14 @@ public class Drawing extends JPanel {
 
 		Graphics2D g2d = (Graphics2D) g;
 
-		if (p[he][k - 1] != 1 && map[1][k - 1] != 9 && Manager.fuel > 0 && Drawing.enemyl < 10) {
+		if (p[he][k - 1] != 1 && map[1][k - 1] != 9 && Manager.fuel > 0 && Drawing.enemy_l < 10) {
 
 			for (int i = 0; i < 7; i++) {
 				for (int j = k - 10; j < k; j++) {
 					if (map[i][j] == 0) {
 						g2d.drawImage(grass, 64 * i, 64 * (j - (k - 9)) + Drawing.t, 64, 64, null);
 					} else if (map[i][j] == 1) {
-						g2d.drawImage(zamin, 64 * i, 64 * (j - (k - 9)) + Drawing.t, 64, 64, null);
+						g2d.drawImage(ground, 64 * i, 64 * (j - (k - 9)) + Drawing.t, 64, 64, null);
 					}
 				}
 			}
@@ -127,23 +135,20 @@ public class Drawing extends JPanel {
 				}
 			}
 
-			for (int i = 0; i < mis.size(); i++) {
-
-				g2d.drawImage(missile, mis.get(i).xLocation * 64 + 28, mis.get(i).ylLocation * 64 - 10, 10, 64, this);
-				if (p[mis.get(i).xLocation][Drawing.k - 1 - mis.get(i).ylLocation + 1] == 1) {
-					p[mis.get(i).xLocation][Drawing.k - 1 - mis.get(i).ylLocation + 1] = 0;
-					Drawing.enemyl++;
-
-
+			for (missiles mi : mis) {
+				g2d.drawImage(missile, mi.xLocation * 64 + 28, mi.ylLocation * 64 - 10, 10, 64, this);
+				if (p[mi.xLocation][Drawing.k - 1 - mi.ylLocation + 1] == 1) {
+					p[mi.xLocation][Drawing.k - 1 - mi.ylLocation + 1] = 0;
+					Drawing.enemy_l++;
 				}
 
 			}
-			g2d.drawImage(myplane, 64 * he, 64 * (8), 64, 64, null);
+			g2d.drawImage(myPlane, 64 * he, 64 * (8), 64, 64, null);
 		} else if (map[1][k - 1] == 9) {
 			g2d.clearRect(0, 0, 1000, 1000);
 			g2d.drawImage(stop, 100, 100, 300, 300, null);
 
-		} else if (Drawing.enemyl >= 10) {
+		} else if (Drawing.enemy_l >= 10) {
 			g2d.clearRect(0, 0, 1000, 1000);
 			g2d.drawImage(win, 100, 100, 300, 300, null);
 		} else {
